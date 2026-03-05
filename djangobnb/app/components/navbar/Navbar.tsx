6dacf600a44bdaed0da5da62f7776732a5c13d27
Anyangos <anyangos851@gmail.com>
2026-03-05 05:36:20 +0300

    Added new files to the components folder

--- djangobnb/app/components/navbar/Navbar.tsx
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import SearchFilters from './SearchFilters';
+import UserNav from './UserNav';
+import AddPropertyButton from './AddPropertyButton';
 
 const Navbar = () => {
     return (
@@ -18,11 +20,13 @@ const Navbar = () => {
                     </Link>
 
                     <div className="flex space-x-6">
-                        <SearchFilters/>
+                        <SearchFilters />
                     </div>
 
                     <div className="flex items-center space-x-6">
-                        Add property - User navid
+                        <AddPropertyButton />
+                         
+                        <UserNav />
                     </div>
                 </div>
             </div>  
